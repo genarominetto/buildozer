d3db9419d22e7c2c61ae91a6f44714d79066b524
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-09-17 00:44:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from kivy.uix.label import Label
 class HelloWorldApp(App):
 
     def build(self):
-        return Label(text='Hello, Worldd  ')
+        return Label(text='Hello, Worldd j ')
 
 if __name__ == '__main__':
     HelloWorldApp().run()
