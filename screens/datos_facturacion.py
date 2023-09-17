a9ccf9b3a3b0151335d62e23a48805357455e1ff
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-09-17 02:16:39 +0200

    Delete screens directory

--- screens/datos_facturacion.py
+++
@@ -1,6 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-class datos_facturacionScreen(Screen):
-    def go_to_cargar_datos(self):
-        print('Navigating from Screen datos_facturacion to Screen cargar_datos')
-        self.manager.current = 'cargar_datos'
--- screens/datos_padres.py
+++
@@ -1,6 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-class datos_padresScreen(Screen):
-    def go_to_cargar_datos(self):
-        print('Navigating from Screen datos_padres to Screen cargar_datos')
-        self.manager.current = 'cargar_datos'
